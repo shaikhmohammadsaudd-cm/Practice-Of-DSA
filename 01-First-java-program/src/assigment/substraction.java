a822c52ed7e70b21dcf356218637b385898933ea
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-20 16:25:31 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/substraction.java
@@ -0,0 +1,12 @@
+package assigment;
+
+public class substraction {
+    static void main(String[] args) {
+        int a=97;
+        int b=90;
+        
+        int c=a-b;
+        System.out.println(c);
+
+    }
+}
