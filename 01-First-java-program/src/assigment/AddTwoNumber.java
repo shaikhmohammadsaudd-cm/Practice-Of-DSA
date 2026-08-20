a1d15d852258636813eb5217aeede7d9ce6ddfe0
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-20 16:20:01 +0530

    start dsa

---
+++ 01-First-java-program/src/assigment/AddTwoNumber.java
@@ -0,0 +1,11 @@
+package assigment;
+
+public class AddTwoNumber {
+    static void main(String[] args) {
+        int a=10;
+        int b=30;
+
+        int c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ 01-First-java-program/src/code/Hello.java
@@ -0,0 +1,8 @@
+package code;
+
+public class Hello {
+    static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("start dsa");
+    }
+}
