9c3167448cb7692ed830b0d0c739dee0e8c42cbb
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-20 18:45:23 +0530

    Add files via upload

---
+++ 01-First-java-program/src/code/DailyCode.java
@@ -0,0 +1,12 @@
+package code;
+
+public class DailyCode {
+    static void main(String[] args) {
+        String title = "Java Practice";
+        int day = 1;
+        int questionSolved = 15;
+        System.out.println("Topic"+title);
+        System.out.println("Day:"+day);
+        System.out.println("Total Solved "+ questionSolved);
+    }
+}
