9808553713217dd91d2a1cbe462d37fb2a97e722
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-22 01:56:25 +0530

    Add files via upload

---
+++ 01-First-java-program/Complement.java
@@ -0,0 +1,9 @@
+package assigment;
+
+public class Complement {
+    static void main(String[] args) {
+        String name = "Bill Gates";
+        String Complement = name+" is an American Businessman";
+        System.out.println(Complement);
+    }
+}
---
+++ 01-First-java-program/MultipleLine.java
@@ -0,0 +1,10 @@
+package assigment;
+
+public class MultipleLine {
+    static void main(String[] args) {
+        System.out.println("Name: Mohammad saud");
+        System.out.println("Course: Engineering");
+        System.out.println("Language: java");
+
+    }
+}
