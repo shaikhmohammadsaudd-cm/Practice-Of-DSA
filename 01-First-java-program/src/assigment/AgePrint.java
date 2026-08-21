c70db951cc45b28196516cd9b2fae93ce4605bd9
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-21 15:34:22 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/AgePrint.java
@@ -0,0 +1,9 @@
+package assigment;
+
+public class AgePrint {
+    static void main(String[] args) {
+        int age = 19;
+        System.out.println("Meri age hai:"+age);
+    }
+
+}
---
+++ 01-First-java-program/src/assigment/AreaOfRectangle.java
@@ -0,0 +1,10 @@
+package assigment;
+
+public class AreaOfRectangle {
+    static void main(String[] args) {
+        int length = 5;
+        int width = 3;
+        int area = length*width;
+        System.out.println("Area Of Rectangle :"+area);
+    }
+}
