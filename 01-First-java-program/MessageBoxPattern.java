145c9b39787ee77179cb438739f975cdcc9991aa
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-23 18:35:51 +0530

    Delete 01-First-java-program/MessageBoxPattern.java

--- 01-First-java-program/MessageBoxPattern.java
+++
@@ -1,9 +0,0 @@
-package assigment;
-
-public class MessageBoxPattern {
-    static void main(String[] args) {
-        System.out.println("+----------+");
-        System.out.println("|   JAVA   |");
-        System.out.println("+----------+");
-    }
-}
