0c1c8a56f45c91504672e223d5902a5182064fe9
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-23 18:35:08 +0530

    Delete 01-First-java-program/Complement.java

--- 01-First-java-program/Complement.java
+++
@@ -1,9 +0,0 @@
-package assigment;
-
-public class Complement {
-    static void main(String[] args) {
-        String name = "Bill Gates";
-        String Complement = name+" is an American Businessman";
-        System.out.println(Complement);
-    }
-}
