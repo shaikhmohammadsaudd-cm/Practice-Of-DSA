9e014796bbf70976be49b3fa6385040103999a7f
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-23 18:42:43 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/MessageBoxPattern.java
@@ -0,0 +1,9 @@
+package assigment;
+
+public class MessageBoxPattern {
+    static void main(String[] args) {
+        System.out.println("+----------+");
+        System.out.println("|   JAVA   |");
+        System.out.println("+----------+");
+    }
+}
