4e934f5891cc2d07026cafa3959b372630dec9ec
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-24 10:49:39 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/Temperature.java
@@ -0,0 +1,9 @@
+package assigment;
+
+public class Temperature {
+    static void main(String[] args) {
+        int celsius = 30;
+        int fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Temperature in Fahrenheit "+fahrenheit);
+    }
+}
---
+++ 01-First-java-program/src/assigment/TextandVariableCombine.java
@@ -0,0 +1,9 @@
+package assigment;
+
+public class TextandVariableCombine {
+    static void main(String[] args) {
+        String name = "Mohammad";
+        int age = 19;
+        System.out.println("mara naam "+name+" My age "+age);
+    }
+}
