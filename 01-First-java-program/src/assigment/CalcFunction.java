c2d5da8391569a03fe4c3ed9c9c293ee003726a7
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-25 01:40:00 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/CalcFunction.java
@@ -0,0 +1,23 @@
+package assigment;
+
+import java.util.Scanner;
+
+public class CalcFunction {
+        static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+
+            char op = sc.next().charAt(0);
+
+            if (op == '=') System.out.println(num1 + num2);
+            else if (op == '-') System.out.println(num1 - num1);
+            else if (op == '*') System.out.println(num1 * num2);
+            else if (op == '/') {
+                if (num1 != 0) System.out.println(num1 / num2);
+                else System.out.println("canot divide by zero");
+            } else {
+                System.out.println("invalid operator");
+            }
+        }
+    }
