fed8707f4fdf402496db4198caf86bfefa7044b3
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-26 00:53:35 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/PersonalDetailsCard.java
@@ -0,0 +1,10 @@
+package assigment;
+
+public class PersonalDetailsCard {
+    static void main(String[] args) {
+        System.out.println("=====================");
+        System.out.println("Name: Mohammad Saud ");
+        System.out.println("Role: Engineering Student");
+        System.out.println("=====================");
+    }
+}
