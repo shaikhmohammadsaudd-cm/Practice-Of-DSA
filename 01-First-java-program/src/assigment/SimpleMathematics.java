4b5f4a2ff4b9b076f90aa120273cc515d7ca93d0
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-26 00:51:24 +0530

    Add files via upload

---
+++ 01-First-java-program/src/assigment/SimpleMathematics.java
@@ -0,0 +1,12 @@
+package assigment;
+
+public class SimpleMathematics {
+    static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction"+(a-b));
+        System.out.println("Division:"+(a/b));
+    }
+}
