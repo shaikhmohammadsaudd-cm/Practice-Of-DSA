7b82248926972e0ff0985c3803de5e9858c39c2b
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-08-27 10:48:53 +0530

    Add files via upload

---
+++ 01-First-java-program/src/code/Detils.java
@@ -0,0 +1,14 @@
+package code;
+
+public class Detils {
+    static void main(String[] args) {
+        String name = "SAUD";
+        String collage = "PES COLLAGE OF ENGGINERING";
+        int fees = 51500;
+        String Year = "Second";
+        int semster = 3;
+        double CGPA =  8.25;
+
+        System.out.println("My Name Is :-"+name+" My Collage Name is :-"+collage+"I Have Paid My Fees Was"+fees+"I have Complete my 1st Year and In Iam Just In Semster ;-"+semster+"My CGPA was :-"+CGPA);
+    }
+}
