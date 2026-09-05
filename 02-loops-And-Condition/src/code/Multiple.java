5ca212b06f4b88acf6765b85ecfece639b16a0b9
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-09-05 17:55:08 +0530

    Add files via upload

---
+++ 02-loops-And-Condition/src/code/Multiple.java
@@ -0,0 +1,21 @@
+package code;
+
+public class Multiple {
+    static void main(String[] args) {
+
+
+    /* it executes one condition from multiple statement
+              Syntax:-
+
+       if(condition 1){
+          code execute if condition 1 is True
+        }else if(condition 2){
+          code execute if condition 2 is true
+        }else{
+           code execute if condition all are false
+        }
+
+     */
+
+    }
+}
