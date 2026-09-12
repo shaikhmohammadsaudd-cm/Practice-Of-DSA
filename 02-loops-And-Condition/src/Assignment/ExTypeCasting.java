d4ffcae95149b28dbc335d239164bd1a9a77195c
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-09-12 13:35:37 +0530

    Add files via upload

---
+++ 02-loops-And-Condition/src/Assignment/ExTypeCasting.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class ExTypeCasting {
+    static void main(String[] args) {
+        Explacite();
+    }
+    static void Explacite(){
+        double x = 10.0;
+        int y = (int)x;
+        System.out.println(y);
+    }
+}
