834535292ba99d04e4d424e110cd2d4f01988e44
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-09-18 10:20:16 +0530

    Add files via upload

---
+++ 02-loops-And-Condition/src/Assignment/LargestThreeNumber.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class LargestThreeNumber {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a");
+        int a = sc.nextInt();
+        System.out.print("Enter b");
+        int b = sc.nextInt();
+        System.out.print("Enter c");
+        int c = sc.nextInt();
+        if(a>=b && a>=c){
+            System.out.println("a largest");
+        } else if (b>=a && b>=c) {
+            System.out.println("b is largest");
+
+        }else {
+            System.out.println("c is largest");
+        }
+
+
+    }
+}
