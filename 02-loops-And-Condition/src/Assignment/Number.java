650ac4d2477289b60b15c60ccdf22fc9476314fa
shaikhmohammadsaudd-cm <shaikh.mohammadsaudd@gmail.com>
2026-09-24 13:19:49 +0530

    Add files via upload

---
+++ 02-loops-And-Condition/src/Assignment/Number.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Number {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num:");
+        int num = sc.nextInt();
+        if(num%5==0&&num%11==0){
+            System.out.println("Divisible ");
+        }else {
+            System.out.println("is not Divisible");
+        }
+    }
+}
